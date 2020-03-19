698af7a0eab8e434efa30b80fdd26d6518959ecd
rémy Lefoll <remy.lefoll98@gmail.com>
2020-03-19 10:48:36 +0100

    Updates

---
+++ precache-manifest.a0c683dec6095fc301d58cefab26308e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03da98cc48a22d8a965bfedd512a7970",
+    "url": "/markdown-app/index.html"
+  },
+  {
+    "revision": "2fb449d933e9bff97915",
+    "url": "/markdown-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "468b2aaf27ab3c18734e",
+    "url": "/markdown-app/static/css/main.14f218a4.chunk.css"
+  },
+  {
+    "revision": "2fb449d933e9bff97915",
+    "url": "/markdown-app/static/js/2.5daf72ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/markdown-app/static/js/2.5daf72ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468b2aaf27ab3c18734e",
+    "url": "/markdown-app/static/js/main.2dae8dbc.chunk.js"
+  },
+  {
+    "revision": "552a4dc890bc7c57beea",
+    "url": "/markdown-app/static/js/runtime-main.950e3c2b.js"
+  }
+]);
